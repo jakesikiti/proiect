bbae35f319f08d355933f023cb7d14dec6b97b2b
Catalin <45767344+jakesikiti@users.noreply.github.com>
2021-06-12 15:12:26 +0300

    Update drum_store.js

--- proiect/drum_store.js
+++ proiect/drum_store.js
@@ -8,6 +8,13 @@
 		document.getElementById("loader").style.display = "none";
 	}
 
+	function after_request(response){
+		  response.headers.add('Access-Control-Allow-Origin', '*');
+		  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+		  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+		  return response;
+	}
+
 	var lista=[
 		];
 		function get(){
